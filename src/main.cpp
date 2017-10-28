408629942712ad7295ff0b8dd690b2f8d0428324
lzjqsdd <lzj7179@163.com>
2017-10-28 11:27:20 +0800

    add tinyxml2 support

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,30 @@
 #include "./core/node.h"
 #include "./core/config.h"
 
+#include "./utils/tinyxml2.h"
+
 using namespace std;
+using namespace tinyxml2;
 
 int main()
 {
     cout << "fight!!!" << endl;
 
     //load config
+    XMLDocument doc;
+    doc.LoadFile("../config/path.xml");
+
+    XMLElement * pathListElement = doc.FirstChildElement("pathlist");
+
+    XMLElement * pathElement= pathListElement->FirstChildElement("path");
+
+    XMLElement *linkElement = pathElement->FirstChildElement("link");
+
+    XMLElement *speedElement = linkElement->FirstChildElement("maxspeed");
+
+    XMLText* speedNode = speedElement->FirstChild()->ToText();
+
+    printf("speed is : %s\n",speedNode->Value());
     //build network
     //init evacuation demand
     //do simulation per step
