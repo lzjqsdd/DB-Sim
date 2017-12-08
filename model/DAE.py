664bbb34881018ada38518c30fdd9891f481789a
lzjqsdd <lzj7179@163.com>
2017-12-08 17:08:49 +0800

    refactor: init data analysis pipeline

--- model/DAE.py
+++ model/DAE.py
@@ -5,30 +5,24 @@ from pandas import DataFrame,Series
 import matplotlib.pyplot as plt
 
 
-col_single_in = ['frame','pre_num','pre_oflow','pre_v','cur_num','cur_inflow','cur_v']
+#根据node列表加载原始数据
+def load_origin_data(data_path, node_list):
+    pass
 
-data_1951 = pd.read_csv("../data/sample/1951_node_sample.txt",delim_whitespace=True,names=col_single_in)
-print (data_1951.describe())
+#数据清洗
+def clean_data(input_data, output_data):
+    #remove NA
+    #remove outlier
+    pass
 
+#分析某个特征和目标变量的关系
+def analysis_data(feature_name,y):
+    pass
 
-## data clean(remove max speed)
-data_1951 = data_1951[data_1951.pre_v <= 40]
+#训练
+def train(train_data):
+    pass
 
-#plt.plot(data_1951.frame[data_1951.pre_v < 40][1:10000],data_1951.pre_v[data_1951.pre_v < 40][1:10000])
-#plt.plot(data_1951.frame[1000:3000], data_1951.pre_v[1000:3000])
-#plt.show()
-
-col_1949 = ['frame','cur_num','cur_inflow','cur_v']
-data_1949 = pd.read_csv("../data/sample/1949_node_sample.txt",delim_whitespace=True,names=col_1949)
-data_1949 = data_1949[data_1949.cur_v < 40]
-print (data_1949.describe())
-
-data_1949 = data_1949[data_1949.cur_v < data_1949.cur_v.max()]
-
-#plt.plot(data_1949.cur_v[5000:10000],'g.-',linewidth=1)
-#plt.show()
-
-print(data_1949.cur_inflow.mean())
-
-plt.scatter(data_1949.frame ,data_1949.cur_inflow)
-plt.show()
+#评分
+def score():
+    pass
---
+++ model/setting.py
@@ -0,0 +1,14 @@
+# 定义全局变量
+
+# 训练数据路径
+train_data_path = '../data/sample/train'
+test_data_path = '../data/sample/test'
+
+# 计算数据缓存位置
+pkl_path = '../data/pkl/'
+
+# 是否进行预处理
+need_preprocess = True
+
+# 处理的node的列表
+node_list = ['1951']
