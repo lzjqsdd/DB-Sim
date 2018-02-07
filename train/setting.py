c751c0eae928e6f87205af7fd3baf5258371bb27
lzjqsdd <lzj7179@163.com>
2018-02-07 11:24:02 +0800

    add model for expand dataset, someting wrong with 1949

--- train/setting.py
+++ train/setting.py
@@ -8,6 +8,17 @@ DEBUG = False
 # root path
 data_root = '../data/one-short-nolight/'
 data_sample_root = '../data/one-short-nolight/one-short-500car-nolight'
+data_sample_root_list = [
+         '../data/one-short-nolight/one-short-500car-nolight',
+         '../data/one-short-nolight/one-short-750car-nolight',
+         '../data/one-short-nolight/one-short-1000car-nolight',
+         '../data/one-short-nolight/one-short-1250car-nolight',
+         '../data/one-short-nolight/one-short-1500car-nolight'
+        ]
+#data_sample_root_list = [
+#         '../data/one-short-nolight/one-short-500car-nolight',
+#        ]
+
 model_root='../model/'
 config_root = '../config'
 train_root = '../train/'
@@ -16,6 +27,11 @@ train_root = '../train/'
 origin_data_path = os.path.join(data_sample_root,'sample/')
 train_data_path = os.path.join(data_sample_root,'sample/train')
 test_data_path = os.path.join(data_sample_root,'sample/test')
+
+# data path list for all condition samples
+origin_data_path_list = []
+for each_sample_root in data_sample_root_list:
+    origin_data_path_list.append(os.path.join(each_sample_root,'sample/'));
 
 # dump path
 pkl_path = os.path.join(train_root,'pkl/')
@@ -27,12 +43,12 @@ data_pkl_filename = os.path.join(pkl_path,'data.pkl')
 need_preprocess = True
 
 # config的文件
-config_file = os.path.join(config_root,"500car-nolight.conf")
+config_file = os.path.join(config_root,"750car-nolight.conf")
 pathxml = os.path.join(data_root,"network/path.xml")
 nodexml = os.path.join(data_root,"network/node.xml")
 
 #model path
-model_version = '20180115'
+model_version = '20180207'
 xgboost_node_model_path = os.path.join(model_root,'xgboost/'+model_version+'/node/')
 xgboost_link_model_path = os.path.join(model_root,'xgboost/'+model_version+'/link/')
 svm_model_path = os.path.join(model_root,'svm')
--- train/train_model.py
+++ train/train_model.py
@@ -11,23 +11,26 @@ def train_for_node1949():
     #step1 load data
     origin_data = []
     #try loading dump data
-    if(os.path.exists(st.data_pkl_filename) and not(st.override_pkl)):
-        origin_data = pickle.load(open(st.data_pkl_filename,'rb'))
-        print("[INFO] load data from : " , st.data_pkl_filename)
-    else:
-        load_origin_data(st.origin_data_path, [1949,1951,2077,102076], origin_data)
-        pickle.dump(origin_data, open(st.data_pkl_filename,'wb'))
-    df_train_1949 = origin_data[0]
+    df_train_1949_all = pd.DataFrame()
 
-    #step2: gen_feature
-    #df_train_1949 = filter_cut(df_train_1949,3000,13500)
-    df_train_1949 = reindex(df_train_1949,1949)
-    df_train_1949 = group_frame(df_train_1949)
-    df_train_1949 = drop_feature(df_train_1949, node_id = 1949)
-    df_train_1949 = gen_label(df_train_1949,node_id = 1949)
+    for data_filename in st.origin_data_path_list:
+        load_origin_data(data_filename, [1949,1951,2077,102076], origin_data)
+        #pickle.dump(origin_data, open(st.data_pkl_filename,'wb'))
+
+        df_train_1949 = origin_data[0]
+
+        #step2: gen_feature
+        df_train_1949 = filter_cut(df_train_1949,3000,13500)
+        df_train_1949 = reindex(df_train_1949,1949)
+        df_train_1949 = group_frame(df_train_1949)
+        df_train_1949 = drop_feature(df_train_1949, node_id = 1949)
+        df_train_1949 = gen_label(df_train_1949,node_id = 1949)
+
+        df_train_1949_all = df_train_1949_all.append(df_train_1949)
 
     #step3: train
-    model_1949 = XGBModel(mid=1949,train_data = df_train_1949, test_data=df_train_1949)
+    print(df_train_1949_all.columns)
+    model_1949 = XGBModel(mid=1949,train_data = df_train_1949_all, test_data=df_train_1949_all)
     param = {'max_depth':3, 'eta':1, 'silent':1, 'objective':'multi:softmax', 'num_class':8}
     model_1949.train(model_path = st.xgboost_node_model_path, param = param)
     model_1949.loadModel(model_path = st.xgboost_node_model_path)
@@ -38,23 +41,23 @@ def train_for_node1951():
     #step1 load data
     origin_data = []
     #try loading dump data
-    if(os.path.exists(st.data_pkl_filename) and not(st.override_pkl)):
-        origin_data = pickle.load(open(st.data_pkl_filename,'rb'))
-        print("[INFO] load data from : " , st.data_pkl_filename)
-    else:
-        load_origin_data(st.origin_data_path, [1949,1951,2077,102076], origin_data)
-        pickle.dump(origin_data, open(st.data_pkl_filename,'wb'))
-    df_train_1951 = origin_data[1]
+    df_train_1951_all = pd.DataFrame()
 
-    #step2: gen_feature
-    df_train_1951 = filter_cut(df_train_1951,2760,13500)
-    df_train_1951 = reindex(df_train_1951,1951)
-    df_train_1951 = group_frame(df_train_1951)
-    df_train_1951 = drop_feature(df_train_1951,node_id = 1951)
-    df_train_1951 = gen_label(df_train_1951,node_id = 1951)
+    for data_filename in st.origin_data_path_list:
+        load_origin_data(data_filename, [1949,1951,2077,102076], origin_data)
+        df_train_1951 = origin_data[1]
+
+        #step2: gen_feature
+        #df_train_1951 = filter_cut(df_train_1951,2760,13500)
+        df_train_1951 = reindex(df_train_1951,1951)
+        df_train_1951 = group_frame(df_train_1951)
+        df_train_1951 = drop_feature(df_train_1951,node_id = 1951)
+        df_train_1951 = gen_label(df_train_1951,node_id = 1951)
+
+        df_train_1951_all = df_train_1951_all.append(df_train_1951)
 
     #step3: train
-    model_1951 = XGBModel(mid=1951,train_data = df_train_1951, test_data=df_train_1951)
+    model_1951 = XGBModel(mid=1951,train_data = df_train_1951_all, test_data=df_train_1951_all)
     param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'multi:softmax', 'num_class':8}
     model_1951.train(model_path = st.xgboost_node_model_path, param = param)
     model_1951.loadModel(model_path = st.xgboost_node_model_path)
@@ -66,24 +69,24 @@ def train_for_node2077():
     #step1 load data
     origin_data = []
     #try loading dump data
-    if(os.path.exists(st.data_pkl_filename) and not(st.override_pkl)):
-        origin_data = pickle.load(open(st.data_pkl_filename,'rb'))
-        print("load data from : " , st.data_pkl_filename)
-    else:
-        load_origin_data(st.origin_data_path, [1949,1951,2077,102076], origin_data)
-        pickle.dump(origin_data, open(st.data_pkl_filename,'wb'))
-    df_train_2077 = origin_data[2]
+    df_train_2077_all = pd.DataFrame()
 
-    #step2: gen_feature
-    df_train_2077= filter_cut(df_train_2077,2760,13500)
-    df_train_2077 = reindex(df_train_2077,2077)
-    df_train_2077 = group_frame(df_train_2077)
-    df_train_2077 = drop_feature(df_train_2077, node_id = 2077)
-    df_train_2077 = gen_label(df_train_2077,node_id = 2077)
+    for data_filename in st.origin_data_path_list:
+        load_origin_data(data_filename, [1949,1951,2077,102076], origin_data)
+        df_train_2077 = origin_data[2]
+
+        #step2: gen_feature
+        #df_train_2077= filter_cut(df_train_2077,2760,13500)
+        df_train_2077 = reindex(df_train_2077,2077)
+        df_train_2077 = group_frame(df_train_2077)
+        df_train_2077 = drop_feature(df_train_2077, node_id = 2077)
+        df_train_2077 = gen_label(df_train_2077,node_id = 2077)
+
+        df_train_2077_all = df_train_2077_all.append(df_train_2077)
 
     #step3: train
     param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'multi:softmax', 'num_class':8}
-    model_2077 = XGBModel(mid=2077,train_data = df_train_2077, test_data=df_train_2077)
+    model_2077 = XGBModel(mid=2077,train_data = df_train_2077_all, test_data=df_train_2077_all)
     model_2077.train(model_path = st.xgboost_node_model_path, param = param)
     model_2077.loadModel(model_path = st.xgboost_node_model_path)
     model_2077.test()
@@ -94,24 +97,24 @@ def train_for_node102076():
     #step1 load data
     origin_data = []
     #try loading dump data
-    if(os.path.exists(st.data_pkl_filename) and not(st.override_pkl)):
-        origin_data = pickle.load(open(st.data_pkl_filename,'rb'))
-        print("load data from : " , st.data_pkl_filename)
-    else:
-        load_origin_data(st.origin_data_path, [1949,1951,2077,102076], origin_data)
-        pickle.dump(origin_data, open(st.data_pkl_filename,'wb'))
-    df_train_102076= origin_data[3]
+    df_train_102076_all = pd.DataFrame()
 
-    #step2: gen_feature
-    df_train_102076 = filter_cut(df_train_102076,2760,13500)
-    df_train_102076= reindex(df_train_102076, 102076)
-    df_train_102076 = group_frame(df_train_102076)
-    df_train_102076 = drop_feature(df_train_102076, node_id = 102076) #剔除某些特征
-    df_train_102076 = gen_label(df_train_102076,node_id=102076)
+    for data_filename in st.origin_data_path_list:
+        load_origin_data(data_filename, [1949,1951,2077,102076], origin_data)
+        df_train_102076= origin_data[3]
+
+        #step2: gen_feature
+        #df_train_102076 = filter_cut(df_train_102076,2760,13500)
+        df_train_102076= reindex(df_train_102076, 102076)
+        df_train_102076 = group_frame(df_train_102076)
+        df_train_102076 = drop_feature(df_train_102076, node_id = 102076) #剔除某些特征
+        df_train_102076 = gen_label(df_train_102076,node_id=102076)
+
+        df_train_102076_all = df_train_102076_all.append(df_train_102076)
 
     #step3: train
     param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'multi:softmax', 'num_class':6}
-    model_102076 = XGBModel(mid=102076,train_data = df_train_102076, test_data=df_train_102076)
+    model_102076 = XGBModel(mid=102076,train_data = df_train_102076_all, test_data=df_train_102076_all)
     model_102076.train(model_path = st.xgboost_node_model_path, param = param)
     model_102076.loadModel(model_path = st.xgboost_node_model_path)
     model_102076.test()
@@ -122,26 +125,27 @@ def train_for_link1949():
     #step1 load data
     origin_data = []
     #try loading dump data
-    if(os.path.exists(st.data_pkl_filename) and not(st.override_pkl)):
-        origin_data = pickle.load(open(st.data_pkl_filename,'rb'))
-        print("[INFO] load data from : " , st.data_pkl_filename)
-    else:
-        load_origin_data(st.origin_data_path, [1949,1951,2077,102076], origin_data)
-        pickle.dump(origin_data, open(st.data_pkl_filename,'wb'))
-    df_train_1949 = origin_data[0]
 
-    #step2: gen_feature
-    df_train_1949 = filter_cut(df_train_1949,2760,13500)
-    df_train_1949 = reindex(df_train_1949,1949)
-    df_train_1949 = group_frame(df_train_1949)
-    df_train_1949 = gen_pool2buffer(df_train_1949,1949)
-    df_train_1949 = drop_feature(df_train_1949, link_id = 1949)
-    df_train_1949 = gen_label(df_train_1949, link_id = 1949)
+    df_train_1949_all = pd.DataFrame()
 
-    print("max label : ",df_train_1949.label.max())
+    for data_filename in st.origin_data_path_list:
+        load_origin_data(data_filename, [1949,1951,2077,102076], origin_data)
+        df_train_1949 = origin_data[0]
+
+        #step2: gen_feature
+        #df_train_1949 = filter_cut(df_train_1949,2760,13500)
+        df_train_1949 = reindex(df_train_1949,1949)
+        df_train_1949 = group_frame(df_train_1949)
+        df_train_1949 = gen_pool2buffer(df_train_1949,1949)
+        df_train_1949 = drop_feature(df_train_1949, link_id = 1949)
+        df_train_1949 = gen_label(df_train_1949, link_id = 1949)
+
+        df_train_1949_all = df_train_1949_all.append(df_train_1949)
+
+    print("max label : ",df_train_1949_all.label.max())
 
     #step3: train
-    model_1949 = XGBModel(mid=1949,train_data = df_train_1949, test_data=df_train_1949)
+    model_1949 = XGBModel(mid=1949,train_data = df_train_1949_all, test_data=df_train_1949_all)
     param = {'max_depth':3, 'eta':1, 'silent':1, 'objective':'multi:softmax', 'num_class':16}
     model_1949.train(model_path = st.xgboost_link_model_path, param = param)
     model_1949.loadModel(model_path = st.xgboost_link_model_path)
@@ -151,26 +155,26 @@ def train_for_link1951():
     #step1 load data
     origin_data = []
     #try loading dump data
-    if(os.path.exists(st.data_pkl_filename) and not(st.override_pkl)):
-        origin_data = pickle.load(open(st.data_pkl_filename,'rb'))
-        print("[INFO] load data from : " , st.data_pkl_filename)
-    else:
-        load_origin_data(st.origin_data_path, [1949,1951,2077,102076], origin_data)
-        pickle.dump(origin_data, open(st.data_pkl_filename,'wb'))
-    df_train_1951 = origin_data[1]
+    df_train_1951_all = pd.DataFrame()
 
-    #step2: gen_feature
-    #df_train_1951 = filter_cut(df_train_1951,2760,13500)
-    df_train_1951 = reindex(df_train_1951,1951)
-    df_train_1951 = group_frame(df_train_1951)
-    df_train_1951 = gen_pool2buffer(df_train_1951, 1951)
-    df_train_1951 = drop_feature(df_train_1951, link_id = 1951)
-    df_train_1951 = gen_label(df_train_1951,link_id = 1951)
+    for data_filename in st.origin_data_path_list:
+        load_origin_data(data_filename, [1949,1951,2077,102076], origin_data)
+        df_train_1951 = origin_data[1]
 
-    print("max label : ", df_train_1951.label.max())
+        #step2: gen_feature
+        #df_train_1951 = filter_cut(df_train_1951,2760,13500)
+        df_train_1951 = reindex(df_train_1951,1951)
+        df_train_1951 = group_frame(df_train_1951)
+        df_train_1951 = gen_pool2buffer(df_train_1951, 1951)
+        df_train_1951 = drop_feature(df_train_1951, link_id = 1951)
+        df_train_1951 = gen_label(df_train_1951,link_id = 1951)
+
+        df_train_1951_all = df_train_1951_all.append(df_train_1951)
+
+    print("max label : ", df_train_1951_all.label.max())
 
     #step3: train
-    model_1951 = XGBModel(mid=1951,train_data = df_train_1951, test_data=df_train_1951)
+    model_1951 = XGBModel(mid=1951,train_data = df_train_1951_all, test_data=df_train_1951_all)
     param = {'max_depth':3, 'eta':1, 'silent':1, 'objective':'multi:softmax', 'num_class':6}
     model_1951.train(model_path = st.xgboost_link_model_path, param = param)
     model_1951.loadModel(model_path = st.xgboost_link_model_path)
@@ -182,27 +186,26 @@ def train_for_link2077():
     #step1 load data
     origin_data = []
     #try loading dump data
-    if(os.path.exists(st.data_pkl_filename) and not(st.override_pkl)):
-        origin_data = pickle.load(open(st.data_pkl_filename,'rb'))
-        print("load data from : " , st.data_pkl_filename)
-    else:
-        load_origin_data(st.origin_data_path, [1949,1951,2077,102076], origin_data)
-        pickle.dump(origin_data, open(st.data_pkl_filename,'wb'))
-    df_train_2077 = origin_data[2]
+    df_train_2077_all = pd.DataFrame()
 
-    #step2: gen_feature
-    df_train_2077= filter_cut(df_train_2077,2760,13500)
-    df_train_2077 = reindex(df_train_2077,2077)
-    df_train_2077 = group_frame(df_train_2077)
-    df_train_2077 = gen_pool2buffer(df_train_2077, 2077)
-    df_train_2077 = drop_feature(df_train_2077, link_id = 2077)
-    df_train_2077 = gen_label(df_train_2077,link_id = 2077)
+    for data_filename in st.origin_data_path_list:
+        load_origin_data(data_filename, [1949,1951,2077,102076], origin_data)
+        df_train_2077 = origin_data[2]
 
+        #step2: gen_feature
+        #df_train_2077= filter_cut(df_train_2077,2760,13500)
+        df_train_2077 = reindex(df_train_2077,2077)
+        df_train_2077 = group_frame(df_train_2077)
+        df_train_2077 = gen_pool2buffer(df_train_2077, 2077)
+        df_train_2077 = drop_feature(df_train_2077, link_id = 2077)
+        df_train_2077 = gen_label(df_train_2077,link_id = 2077)
+
+        df_train_2077_all = df_train_2077_all.append(df_train_2077)
 
     print("max label : ", df_train_2077.label.max())
     #step3: train
     param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'multi:softmax', 'num_class':11}
-    model_2077 = XGBModel(mid=2077,train_data = df_train_2077, test_data=df_train_2077)
+    model_2077 = XGBModel(mid=2077,train_data = df_train_2077_all, test_data=df_train_2077_all)
     model_2077.train(model_path = st.xgboost_link_model_path, param = param)
     model_2077.loadModel(model_path = st.xgboost_link_model_path)
     model_2077.test()
